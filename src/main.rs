59c6374ba0b79babd6733c40ba21e1931b24b55d
Björn Domars <bdomars@walkbase.com>
2022-06-14 15:11:24 +0300

    Print temperature for ruuvi tags found

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+// See the "macOS permissions note" in README.md before running this on macOS
+// Big Sur or later.
+
+use btleplug::api::{Central, CentralEvent, Manager as _, ScanFilter};
+use btleplug::platform::{Adapter, Manager};
+use futures::stream::StreamExt;
+use std::error::Error;
+use ruuvi_sensor_protocol::{SensorValues, Temperature, MacAddress};
+
+async fn get_central(manager: &Manager) -> Adapter {
+    let adapters = manager.adapters().await.unwrap();
+    adapters.into_iter().nth(0).unwrap()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let manager = Manager::new().await?;
+
+    // get the first bluetooth adapter
+    // connect to the adapter
+    let central = get_central(&manager).await;
+
+    // Each adapter has an event stream, we fetch via events(),
+    // simplifying the type, this will return what is essentially a
+    // Future<Result<Stream<Item=CentralEvent>>>.
+    let mut events = central.events().await?;
+
+    // start scanning for devices
+    central.start_scan(ScanFilter::default()).await?;
+
+    // Print based on whatever the event receiver outputs. Note that the event
+    // receiver blocks, so in a real program, this should be run in its own
+    // thread (not task, as this library does not yet use async channels).
+    while let Some(event) = events.next().await {
+        match event {
+            CentralEvent::ManufacturerDataAdvertisement {
+                id: _,
+                manufacturer_data,
+            } => {
+                if let Some(ruuvidata) = &manufacturer_data.get(&0x0499) {
+                    let sensordata = SensorValues::from_manufacturer_specific_data(0x0499, &ruuvidata)?;
+                    println!("{:}C at {:x?}", (sensordata.temperature_as_millicelsius().unwrap() as f64)/1000.0, sensordata.mac_address().unwrap());
+                }
+            }
+            _ => {}
+        }
+    }
+    Ok(())
 }
