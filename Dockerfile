cfc4f760844694957561925431e6a240866aa891
Björn Domars <bdomars@walkbase.com>
2022-06-15 16:36:39 +0300

    quick names hack

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM ghcr.io/cross-rs/aarch64-unknown-linux-gnu:main
+
+# Install libdbus libraries and pkg-config
+RUN dpkg --add-architecture arm64	 && \
+	    apt-get update && \
+	    apt-get install --assume-yes libdbus-1-dev libdbus-1-dev:arm64 pkg-config
--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,8 @@ struct RuuviTag {
 
 impl Display for RuuviTag {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let temperature = (self.sensorvalues.temperature_as_millicelsius().unwrap() as f64) / 1000.0;
+        write!(f, "{} ({})\t{}", self.name, self.address, temperature)?;
         Ok(())
     }
 }
@@ -30,7 +32,7 @@ async fn get_central(manager: &Manager) -> Adapter {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     
-    let knownSensors: HashMap<HwAddr, String> = HashMap::from([
+    let known_sensors: HashMap<HwAddr, String> = HashMap::from([
         (HwAddr::from([0xc4, 0xb, 0x5, 0xbd, 0xe9, 0xd5]), String::from("Meltdown Britney")),
         (HwAddr::from([0xd1, 0xd8, 0x9a, 0xe1, 0x46, 0x3a]), String::from("Climate Change Joe")),
     ]);
@@ -61,9 +63,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
             } => {
                 if let Some(ruuvidata) = &manufacturer_data.get(&0x0499) {
                     let sensordata = SensorValues::from_manufacturer_specific_data(0x0499, &ruuvidata)?;
+                    let hwaddr = HwAddr::from(sensordata.mac_address().unwrap());
+
                     let ruuvitag = RuuviTag{
-                        name: knownSensors.get(sensordata.mac_address().unwrap())
-                    }
+                        name: known_sensors.get(&hwaddr).unwrap_or(&String::from("unknown")).to_owned(),
+                        address: hwaddr,
+                        sensorvalues: sensordata,
+                    };
+
+                
+
+                    println!("{}", ruuvitag)
                 }
             }
             _ => {}
