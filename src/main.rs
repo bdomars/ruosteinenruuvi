124f2cbd77d0f64132c285da48e6446f694ea63a
Björn Domars <bdomars@walkbase.com>
2022-06-16 15:31:22 +0300

    remove whitespace

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@
 use btleplug::api::{Central, CentralEvent, Manager as _, ScanFilter};
 use btleplug::platform::{Adapter, Manager};
 use futures::stream::StreamExt;
-use std::error::Error;
-use ruuvi_sensor_protocol::{SensorValues, Temperature, MacAddress, MeasurementSequenceNumber};
-use std::fmt::Display;
-use std::collections::HashMap;
 use hwaddr::HwAddr;
+use ruuvi_sensor_protocol::{MacAddress, MeasurementSequenceNumber, SensorValues, Temperature};
+use std::collections::HashMap;
+use std::error::Error;
+use std::fmt::Display;
 
 struct RuuviTag {
     name: String,
@@ -18,9 +18,15 @@ struct RuuviTag {
 
 impl Display for RuuviTag {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let temperature = (self.sensorvalues.temperature_as_millicelsius().unwrap() as f64) / 1000.0;
+        let temperature =
+            (self.sensorvalues.temperature_as_millicelsius().unwrap() as f64) / 1000.0;
         let seqnr = self.sensorvalues.measurement_sequence_number().unwrap_or(0);
-        write!(f, "{} ({})\tm#{}\t{}°C", self.name, self.address, seqnr, temperature)?;
+
+        write!(
+            f,
+            "{} ({})\tm#{}\t{}°C",
+            self.name, self.address, seqnr, temperature
+        )?;
         Ok(())
     }
 }
@@ -32,12 +38,16 @@ async fn get_central(manager: &Manager) -> Adapter {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    
     let known_sensors = HashMap::from([
-        (HwAddr::from([0xc4, 0xb, 0x5, 0xbd, 0xe9, 0xd5]), String::from("Meltdown Britney")),
-        (HwAddr::from([0xd1, 0xd8, 0x9a, 0xe1, 0x46, 0x3a]), String::from("Climate Change Joe")),
+        (
+            HwAddr::from([0xc4, 0xb, 0x5, 0xbd, 0xe9, 0xd5]),
+            String::from("Meltdown Britney"),
+        ),
+        (
+            HwAddr::from([0xd1, 0xd8, 0x9a, 0xe1, 0x46, 0x3a]),
+            String::from("Climate Change Joe"),
+        ),
     ]);
-
 
     let manager = Manager::new().await?;
 
@@ -63,17 +73,18 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 manufacturer_data,
             } => {
                 if let Some(ruuvidata) = &manufacturer_data.get(&0x0499) {
-                    let sensordata = SensorValues::from_manufacturer_specific_data(0x0499, &ruuvidata)?;
+                    let sensordata =
+                        SensorValues::from_manufacturer_specific_data(0x0499, &ruuvidata)?;
                     let hwaddr = HwAddr::from(sensordata.mac_address().unwrap());
 
-                    let ruuvitag = RuuviTag{
-                        name: known_sensors.get(&hwaddr).unwrap_or(&String::from("unknown")).to_owned(),
+                    let ruuvitag = RuuviTag {
+                        name: known_sensors
+                            .get(&hwaddr)
+                            .unwrap_or(&String::from("unknown"))
+                            .to_owned(),
                         address: hwaddr,
                         sensorvalues: sensordata,
                     };
-
-                
-
                     println!("{}", ruuvitag)
                 }
             }
