11c99c529dda08a300e3eeac6e12823dfca3ef30
Björn Domars <bdomars@walkbase.com>
2022-07-01 23:49:13 +0300

    btle + channel working

---
+++ src/btle.rs
@@ -0,0 +1,50 @@
+use btleplug::api::{Central, CentralEvent, Manager as _, ScanFilter};
+use btleplug::platform::{Adapter, Manager};
+use futures::stream::StreamExt;
+use ruuvi_sensor_protocol::SensorValues;
+use tokio::sync::broadcast::Sender;
+use std::error::Error;
+
+use crate::RuuviMessage;
+
+const RUUVI: u16 = 0x499;
+
+async fn get_central(manager: &Manager) -> Adapter {
+    let adapters = manager.adapters().await.unwrap();
+    adapters.into_iter().nth(0).unwrap()
+}
+
+pub async fn scan_btle(tx: Sender<RuuviMessage>) -> Result<(), Box<dyn Error>> {
+
+    let manager = Manager::new().await?;
+    let central = get_central(&manager).await;
+    let mut events = central.events().await?;
+    central.start_scan(ScanFilter::default()).await?;
+    while let Some(event) = events.next().await {
+        match event {
+            CentralEvent::ManufacturerDataAdvertisement {
+                id,
+                manufacturer_data,
+            } => {
+                println!(
+                    "ManufacturerDataAdvertisement: {:?}, {:?}",
+                    id, manufacturer_data
+                );
+
+                if let Some(md) = manufacturer_data.get(&RUUVI) {
+                    let sv = match SensorValues::from_manufacturer_specific_data(RUUVI, md) {
+                        Ok(sv) => sv,
+                        Err(err) => {
+                            println!("could not decode sensor data: {}", err);
+                            continue
+                        }
+                    };
+                    let rm = RuuviMessage::from(sv);
+                    tx.send(rm)?;
+                }
+            }
+            _ => {}
+        }
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error};
+
+use ruuvi::scan_btle;
+use tokio::{sync::broadcast};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let (tx, mut rx) = broadcast::channel(16);
+
+    tokio::spawn(async move {
+        let r = scan_btle(tx).await;
+        if r.is_err() {
+            println!("defuk? {}", r.unwrap_err());
+        }
+    });
+
+    loop {
+        let rm = rx.recv().await;
+        println!("Got a ruuvimessage:");
+        println!("{:#?}", rm);
+    }
 }
