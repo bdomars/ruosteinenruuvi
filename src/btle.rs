26879b1ee4342194388a7f0c5487593b4703ba2c
Björn Domars <bdomars@walkbase.com>
2022-07-01 23:54:43 +0300

    remove debug print and some warnings

--- src/btle.rs
+++ src/btle.rs
@@ -15,7 +15,6 @@ async fn get_central(manager: &Manager) -> Adapter {
 }
 
 pub async fn scan_btle(tx: Sender<RuuviMessage>) -> Result<(), Box<dyn Error>> {
-
     let manager = Manager::new().await?;
     let central = get_central(&manager).await;
     let mut events = central.events().await?;
@@ -23,14 +22,9 @@ pub async fn scan_btle(tx: Sender<RuuviMessage>) -> Result<(), Box<dyn Error>> {
     while let Some(event) = events.next().await {
         match event {
             CentralEvent::ManufacturerDataAdvertisement {
-                id,
+                id: _,
                 manufacturer_data,
             } => {
-                println!(
-                    "ManufacturerDataAdvertisement: {:?}, {:?}",
-                    id, manufacturer_data
-                );
-
                 if let Some(md) = manufacturer_data.get(&RUUVI) {
                     let sv = match SensorValues::from_manufacturer_specific_data(RUUVI, md) {
                         Ok(sv) => sv,
