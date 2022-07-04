2d70b607be75f95b24e00b4e4649466e73fa8967
Björn Domars <bdomars@walkbase.com>
2022-07-04 14:52:28 +0300

    test parse Tags.toml file

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
+use std::{fs, vec::Vec};
+
+use serde::{Deserialize, Serialize};
 use ruuvi::scan_btle;
 use tokio::{sync::broadcast};
+use macaddr::MacAddr6;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Tag {
+    name: String,
+    #[serde(with = "serde_with::rust::display_fromstr")]
+    address: MacAddr6,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Config {
+    tags: Vec<Tag>,
+}
 
 #[tokio::main]
 async fn main() {
     let (tx, mut rx) = broadcast::channel(16);
 
     tokio::spawn(async move {
-        let r = scan_btle(tx).await;
-        if r.is_err() {
-            println!("defuk? {}", r.unwrap_err());
-        }
+        match scan_btle(tx).await {
+            Ok(_) => println!("scan_btle completed with ok!"),
+            Err(e) => println!("scan_btle completed with error: {}", e),
+        };
     });
+
+    let contents = fs::read_to_string("Tags.toml")
+    .expect("Something went wrong reading the file");
+
+    //let config: Config = toml::from_str(&contents).unwrap();
+    let config: Config = toml::from_str(&contents).unwrap();
+    println!("{:#?}", config);
 
     loop {
         let rm = rx.recv().await;
